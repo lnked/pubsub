705cfc98899576a961a40fcf07fb31b33f14f35c
ed <ed.proff@gmail.com>
2017-10-18 02:50:45 +0300

    fix example.php

--- example.php
+++ example.php
@@ -4,29 +4,29 @@
 include __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'pubsub.php';
 
 // Basic usage
-PS::subscribe('beforeSave', function ($message) {
+PS::on('beforeSave', function ($message) {
     echo 'PS::beforeSave', '<br>', print_r($message), '<br>', '<br>';
 });
 
-PS::subscribe('beforeSave', function ($message) {
+PS::on('beforeSave', function ($message) {
     echo 'PS::beforeSave', '<br>', print_r($message), '<br>', '<br>';
 });
 
-PS::subscribe('beforeSave', function ($message) {
+PS::on('beforeSave', function ($message) {
     echo 'PS::beforeSave', '<br>', print_r($message), '<br>', '<br>';
 });
 
-PS::subscribe('afterSave', function ($message) {
+PS::on('afterSave', function ($message) {
     echo 'PS::afterSave', '<br>', print_r($message), '<br>', '<br>';
 });
 
 // 
-PS::publish('beforeSave', 'test-before');
-PS::publish('afterSave', 'test-after');
-PS::publish('afterSave');
+PS::trigger('beforeSave', 'test-before');
+PS::trigger('afterSave', 'test-after');
+PS::trigger('afterSave');
 
 // 
-PS::unsubscribe('beforeSave');
+PS::off('beforeSave');
 
 // 
 PS::flush();
